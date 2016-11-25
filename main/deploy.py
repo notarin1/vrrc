34eec5202eb7272f2dc1a6ed0c9b82b5c20e7d90
Yuta Hongo <yutago@gmail.com>
2016-11-25 15:29:34 +0900

    Comment out deploy codes

--- main/deploy.py
+++ main/deploy.py
@@ -1,13 +1,11 @@
-import subprocess
-
-def deploy(channel):
-    try:
-        if channel==24:
-            deploy = ['cd /home/pi/vrrc; git pull']
-            restart = ['systemctl restart vrrcd.service']
-            print(subprocess.check_output(deploy, shell=True))
-            print(subprocess.check_output(restart, shell=True))                       
-    except subprocess.CalledProcessError as e:
-        print(e.returncode)
-        print(e.cmd)
-        print(e.output)
+# def deploy(channel):
+#     try:
+#         if channel==24:
+#             deploy = ['cd /home/pi/vrrc; git pull']
+#             restart = ['systemctl restart vrrcd.service']
+#             print(subprocess.check_output(deploy, shell=True))
+#             print(subprocess.check_output(restart, shell=True))
+#     except subprocess.CalledProcessError as e:
+#         print(e.returncode)
+#         print(e.cmd)
+#         print(e.output)
