699442811807a3f47809aba118e8df44092292c9
hirakida <hirakida29@gmail.com>
2016-11-25 19:46:08 +0900

    add sticker api

--- main/http_client.py
+++ main/http_client.py
@@ -15,3 +15,27 @@ def send_message(text):
         validate_cert=False)
     client = tornado.httpclient.AsyncHTTPClient()
     client.fetch(request)
+
+
+def send_alert_sticker():
+    send("alert")
+
+
+def send_start_sticker():
+    send("start")
+
+
+def send_stop_sticker():
+    send("stop")
+
+
+def send(path):
+    url = "https://line-bot-hirakida.herokuapp.com/api/stickerMessage/" + path
+    headers = tornado.httputil.HTTPHeaders({"content-type": "application/json; charset=utf-8"})
+    request = tornado.httpclient.HTTPRequest(
+        url=url,
+        method="POST",
+        headers=headers,
+        validate_cert=False)
+    client = tornado.httpclient.AsyncHTTPClient()
+    client.fetch(request)
